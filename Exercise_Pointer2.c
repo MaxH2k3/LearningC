df7433824bdbed8e77353dbd7eea4f574aa2fb25
MaxH2k3 <huy110903@gmail.com>
2021-10-10 09:56:00 +0700

    update1

--- Exercise_Pointer2.c
+++ Exercise_Pointer2.c
@@ -1,35 +1,38 @@
 #include <stdio.h>
 
-struct hocsinh {
+typedef struct {
     char * name;
     float math_grade;
     float english_grade;
     float literature_grade;
-};
+    float average;
+} hocsinh;
 
 int main() {
-    struct hocsinh hs1;
+    hocsinh hs1;
     hs1.name = "Le Huy";
     hs1.math_grade = 10.0;
     hs1.english_grade = 9.2;
     hs1.literature_grade = 8.1;
+    hs1.average = (hs1.english_grade + hs1.literature_grade + hs1.math_grade) / 3;
 
 
-    struct hocsinh hs2;
+    hocsinh hs2;
     hs2.name = "Nguyen Duy";
     hs2.math_grade = 5.2;
     hs2.english_grade = 4.2;
     hs2.literature_grade = 2.9;
+    hs2.average = (hs2.english_grade + hs2.literature_grade + hs2.math_grade) / 3;
 
-
-    struct hocsinh hs3;
+    hocsinh hs3;
     hs3.name = "Tran Dan";
     hs3.math_grade = 7.2;
     hs3.english_grade = 6.9;
     hs3.literature_grade = 8.1;
+    hs3.average = (hs3.english_grade + hs3.literature_grade + hs3.math_grade) / 3;
 
     
-    struct hocsinh class_speacial[3] = {
+    hocsinh class_speacial[3] = {
         hs1,
         hs2,
         hs3
@@ -37,8 +40,11 @@ int main() {
 
     for (int i = 0; i < 3; i++)
     {
-        struct hocsinh hs = class_speacial[i];
-        printf("My name is %s\n. Point of subject math = %f, english = %f, literature = %f\n", hs.name, hs.math_grade, hs.english_grade, hs.literature_grade);
+        hocsinh hs = class_speacial[i];
+        printf("My name is %s\n", hs.name);
+        printf("Point of subject math = %f, english = %f, literature = %f\n", hs.math_grade, hs.english_grade, hs.literature_grade);
+        printf("Average of subject = %f\n", hs.average);
+        printf("-----------------\n");
     }
     
 
